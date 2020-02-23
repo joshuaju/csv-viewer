9bf8800ccc57b31d954f0531c27c3a53db641298
Joshua Jungen <joshua.jungen@outlook.de>
2020-02-23 19:19:54 +0100

    pom configuration, removal of not needed constructors

--- src/main/java/de/ccd/training/data/Record.java
+++ src/main/java/de/ccd/training/data/Record.java
@@ -3,7 +3,6 @@ package de.ccd.training.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.csv.CSVRecord;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,7 +14,7 @@ public class Record {
 
     private List<String> values = new ArrayList<>();
 
-    public Record(String... values){
+    public Record(String... values) {
         this.values = Arrays.asList(values);
     }
 }
